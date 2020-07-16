155f8093b4602a5d1d1b5879cd4255119a9f8007
Ebo Lee <yiboli.coding@gmail.com>
2020-07-16 00:44:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,17 +4,17 @@
 
 def key_for_min_value(name_hash)
  
-    min = 9999999999999999999999
-    keep = ''
-    name_hash.collect do |key, value|
-        if value < min
+    min = nil
+    min_key = nil
+    name_hash.each do |key, value|
+        if !min || value < min
           min = value
-          keep = key
+          min_key = key
         end
     end
     if name_hash.length == 0
       nil
     else
-      keep
+      min_key
     end
 end
